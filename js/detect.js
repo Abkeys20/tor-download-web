7344173e5b572ea353bbf2acf27991a01cd021c2
William Papper <contact@papper.me>
2014-03-29 16:42:20 -0400

    Added a stub for detecting values

---
+++ js/detect.js
@@ -0,0 +1,12 @@
+// Detects language, OS version, and architecture to pre-select the dropdowns
+function detectLanguage() {
+	
+}
+
+function detectOS() {
+	
+}
+
+function detectArchitecture() {
+	
+}
