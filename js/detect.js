f1ba6039e7708c36efb23978b298ef91742ad194
William Papper <contact@papper.me>
2014-03-29 17:18:19 -0400

    Implemented detectOS()
    See
    https://stackoverflow.com/questions/19877924/what-is-the-list-of-possibl
    e-values-for-navigator-platform-as-of-today for a complete list of
    values

--- js/detect.js
+++ js/detect.js
@@ -1,12 +1,20 @@
 // Detects language, OS version, and architecture to pre-select the dropdowns
 function detectLanguage() {
-	
+
 }
 
 function detectOS() {
-	
+	if (navigator.platform.toLowerCase().indexOf("win") != -1) {
+		return "Windows";
+	}
+	if (navigator.platform.toLowerCase().indexOf("mac") != -1) {
+		return "Apple OS X";
+	}
+	if (navigator.platform.toLowerCase().indexOf("linux") != -1 || navigator.platform.toLowerCase.indexOf("x11") != -1 || navigator.platform.toLowerCase.indexOf("bsd") != -1) {
+		return "Unix";
+	}
 }
 
 function detectArchitecture() {
-	
+
 }
