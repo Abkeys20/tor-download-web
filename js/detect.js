038aa98a34e9f07c962ece3a4e3e0fe231653c29
William Papper <contact@papper.me>
2014-03-29 22:41:08 -0400

    Added an explanation for the implementation of detectOS()

--- js/detect.js
+++ js/detect.js
@@ -4,6 +4,8 @@ function detectLanguage() {
 }
 
 function detectOS() {
+	// This method is chosen instead of Stoimen's browser detection library, which is used on the current Tor Project download-easy page (found here: http://www.stoimen.com/blog/2009/07/16/jquery-browser-and-os-detection-plugin/).
+	// There's no need for us to use browser detection, so we can just detect the OS here instead.
 	if (navigator.platform.toLowerCase().indexOf("win") != -1) {
 		return "Windows";
 	}
