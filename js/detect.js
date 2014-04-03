18aca4f220ed284dac6abac4be845a100d5a0c04
oneski <davidesilverman@yahoo.com>
2014-04-02 20:45:37 -0400

    Bug Fix

--- js/detect.js
+++ js/detect.js
@@ -1,7 +1,7 @@
 // Pre-selects the dropdown defaults using jQuery and automatically changes the download button URL
 $(document).ready(function() {
 	preSelect();
-}
+
 });
 
 function preSelect() {
