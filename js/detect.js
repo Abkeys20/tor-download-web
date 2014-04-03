3291e62968acb457fec2d3bc6e3b5b2a4a433eaa
William Papper <contact@papper.me>
2014-04-03 11:46:43 -0400

    Updated return values to Linux/BSD from Unix

--- js/detect.js
+++ js/detect.js
@@ -46,10 +46,10 @@ function detectOS() {
 	} else if (platform.indexOf("linux") !== -1 || platform.indexOf("x11") !== -1 || platform.indexOf("bsd") !== -1) {
         if (architecture !== undefined) {
             if (architecture.indexOf("32-bit") !== -1) {
-                return "Unix 32-bit";
+                return "Linux/BSD 32-bit";
             }
             else if (architecture.indexOf("64-bit") !== -1) {
-                return "Unix 64-bit";
+                return "Linux/BSD 64-bit";
             }
             else {
                 return null;
@@ -108,9 +108,9 @@ function setDownload(language, os) {
 	// else if (os.indexOf("Apple OS X") != -1 && architecture.indexOf("64-bit") != -1) {
 	// $("#download-url").val(os).attr("href", osxtbb64);
 	// }
-	else if (os.indexOf("Unix 32-bit") !== -1) {
+	else if (os.indexOf("Linux/BSD 32-bit") !== -1) {
 		$("#download-url").val(os).attr("href", lintbb32);
-	} else if (os.indexOf("Unix 64-bit") !== -1) {
+	} else if (os.indexOf("Linux/BSD 64-bit") !== -1) {
 		$("#download-url").val(os).attr("href", lintbb64);
 	}
 	// Make the language code human-readable
