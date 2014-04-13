fe0345b696f75fb67ad066cb6a6bf91a1ad7b0d0
William Papper <contact@papper.me>
2014-04-12 22:24:09 -0400

    Made download start on the thank-you page
    This allows the back button to function properly.
    
    It also has the benefit of allowing a user to pass a direct link to a
    friend, removing the need for someone to even go through the drop
    downs. This setup is a good balance between a direct link (where there
    is no information provided) and the drop-downs (which a friend may have
    already filled out when linking).

---
+++ js/thank-you.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    detectParameters();
+});
+
+function detectParameters() {
+    var os = getQueryVariable("os");
+    var language = getQueryVariable("lang");
+    setDownload(language, os);
+}
+
+function setDownload(language, os) {
+    // Values taken from the source code of https://www.torproject.org/download/download.html.en
+    var wintbb = 'https://www.torproject.org/dist/torbrowser/3.5.4/torbrowser-install-3.5.4_' + language + '.exe';
+    var wintbbsig = 'https://www.torproject.org/dist/torbrowser/3.5.4/torbrowser-install-3.5.4_' + language + '.exe.asc';
+    var osxtbb32 = 'https://www.torproject.org/dist/torbrowser/3.5.4/TorBrowserBundle-3.5.4-osx32_' + language + '.zip';
+    var osxtbb32sig = 'https://www.torproject.org/dist/torbrowser/3.5.4/TorBrowserBundle-3.5.4-osx32_' + language + '.zip.asc';
+    // Apparently there is no 64-bit OS X download, even though it's in the source of the Tor Project's "All Downloads" page:
+    // var osxtbb64 = 'https://www.torproject.org/dist/torbrowser/3.5.4/TorBrowserBundle-3.5.4-osx64_' + language + '.zip';
+    var lintbb32 = 'https://www.torproject.org/dist/torbrowser/3.5.4/tor-browser-linux32-3.5.4_' + language + '.tar.xz';
+    var lintbb32sig = 'https://www.torproject.org/dist/torbrowser/3.5.4/tor-browser-linux32-3.5.4_' + language + '.tar.xz.asc';
+    var lintbb64 = 'https://www.torproject.org/dist/torbrowser/3.5.4/tor-browser-linux64-3.5.4_' + language + '.tar.xz';
+    var lintbb64sig = 'https://www.torproject.org/dist/torbrowser/3.5.4/tor-browser-linux64-3.5.4_' + language + '.tar.xz.asc';
+    if (os !== undefined && os !== null) {
+        if (os.indexOf("windows") !== -1) {
+            $("#download-url").val(os).attr("src", wintbb);
+            $("#sig-url").val(os).attr("href", wintbbsig);
+        } else if (os.indexOf("osx") !== -1) {
+            $("#download-url").val(os).attr("src", osxtbb32);
+            $("#sig-url").val(os).attr("href", osxtbb32sig);
+        }
+        // No 64-bit OS X version seems to exist
+        // else if (os.indexOf("Apple OS X") != -1 && architecture.indexOf("64-bit") != -1) {
+        // $("#download-url").val(os).attr("href", osxtbb64);
+        // }
+        else if (os.indexOf("linux32") !== -1) {
+            $("#download-url").val(os).attr("src", lintbb32);
+            $("#sig-url").val(os).attr("href", lintbb32sig);
+        } else if (os.indexOf("linux64") !== -1) {
+            $("#download-url").val(os).attr("src", lintbb64);
+            $("#sig-url").val(os).attr("href", lintbb64sig);
+        }
+    }
+}
+
+// Function is from here: http://css-tricks.com/snippets/javascript/get-url-variables/
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
