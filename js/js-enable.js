6f49f65d4476606e077d4e2ba529b49d74621dbd
William Papper <contact@papper.me>
2014-04-18 17:16:58 -0400

    Added a fix for caching issues

--- js/js-enable.js
+++ js/js-enable.js
@@ -2,3 +2,7 @@ $(document).ready(function() {
 	$('#javascript-off-wrapper').hide();
 	$('#javascript-on-wrapper').show();
 });
+
+$(window).bind("unload", function() {
+   // Stub method to prevent errors from caching 
+});
