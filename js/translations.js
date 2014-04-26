493341bd77b8d5a778b82120cad5488b4395bf72
oneski <davidesilverman@yahoo.com>
2014-04-26 10:52:59 -0400

    Created Guide for Localization

--- js/translations.js
+++ js/translations.js
@@ -1,1 +1,180 @@
 // Translations for all languages are included:
+//Word for word translations are not necessary, feel free to simplify (ex: "Experimental Browser" instead of "Want to test a beta Tor Browser?")
+//For Arabic and Persian please use this tool to convert text to unicode character codes (decimal please): 
+//http://www.endmemo.com/unicode/unicodeconverter.php
+
+//Download Tor Browser
+/*
+en-US: Download Tor Browser
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
+
+//Download
+/*
+en-US: Download
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
+
+//Looking for something else? Select other options
+/*
+en-US: Looking for something else? Select other options
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
+
+//About Tor
+/*
+en-US: About Tor
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
+
+//Donate
+/*
+en-US: Donate
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
+
+//Want to test a beta Tor Browser?
+/*
+en-US: Want to test a beta Tor Browser?
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
+
+//Thank You for Downloading
+/*
+en-US: Thank You for Downloading
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
+
+//Verify Signatures
+/*
+en-US: Verify Signatures
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
+
+//What is this?
+/*
+en-US: What is this?
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
+
+//Other Tor Services
+/*
+en-US: Other Tor Services
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
+
+//Help Using Tor
+/*
+en-US: Help Using Tor
+ar: String.fromCharCode()
+de:
+es-ES:
+fa: String.fromCharCode()
+fr:
+it:
+pl:
+pt-PT:
+ru:
+vi:
+zh-CH:
+*/
