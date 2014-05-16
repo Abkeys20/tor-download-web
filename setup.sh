3898243f6b691e2bf81fe823c441d0358fcdc908
William Papper <contact@papper.me>
2014-05-15 23:37:33 -0400

    Added conditionals involving ls

--- setup.sh
+++ setup.sh
@@ -12,12 +12,13 @@ cd $TARGET_DIR
 
 # If there is an update, check for the latest version of the Tor Browser
 cd $DIST_FOLDER
-ls
-
-# Check the GPG signatures. IMPORTANT: ONLY CONTINUE IF THE SIGNATURES CAN BE SUCCESSFULLY VERIFIED. Otherwise, print an error.
-
-# Create mirror/ and move the version directory (e.g. 3.6) into the folder (so the new path is mirror/3.6/)
-
-# Update the URLs in thank-you.js to point to the new files on the mirror
+VERSION=`ls`
+if [ ! -z $VERSION ]
+then
+  # Check the GPG signatures. IMPORTANT: ONLY CONTINUE IF THE SIGNATURES CAN BE SUCCESSFULLY VERIFIED. Otherwise, print an error.
+  
+  # Create mirror/ and move the version directory (e.g. 3.6) into the folder (so the new path is mirror/3.6/)
+  
+  # Update the URLs in thank-you.js to point to the new files on the mirror
 
 # Add auto-updating to cron to run at a specified interval (One hour? Three hours? Six hours?)
